1c02e46c46f3a9cfc0d9f3d86c9fb99a63e304c4
MBIANDI <mbialaura12@gmail.com>
2024-10-27 14:34:12 +0100

    create a ressource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,8 @@
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "my_rg" {
+  name     = "my-terraform-resources"   # Name of the resource group
+  location = "East US"                  # Azure region where you want the resources
+}
